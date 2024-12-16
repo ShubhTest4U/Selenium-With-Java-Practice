022160f7e8b84c7479d7120ddd102a7e1a5ce32b
ShubhTest4u <shubhamkori63@gmail.com>
2024-12-16 20:42:14 +0530

    Static select DropDown

---
+++ Static_Select_DropDown.java
@@ -0,0 +1,32 @@
+package selenium_Learning;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Static_Select_DropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.manage().window().maximize();
+		WebElement StaticSelect = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+
+		Select dropdown = new Select(StaticSelect);
+		dropdown.selectByIndex(2);
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+		dropdown.selectByVisibleText("AED");
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+		dropdown.selectByValue("USD");
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+		Thread.sleep(3000);
+
+		driver.quit();
+
+	}
+
+}
