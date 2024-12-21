0b239c0577f19fd9da5c1a0a33565d2ecb189fea
ShubhTest4u <shubhamkori63@gmail.com>
2024-12-21 23:49:23 +0530

    Navigation

--- Navigation.java
+++ Navigation.java
@@ -1,18 +1,15 @@
-package SeleniumLearning;
+package selenium_Learning;
 
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class Navigation {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
-		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://www.amazon.in/");
